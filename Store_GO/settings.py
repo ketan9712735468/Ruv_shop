400e5b2e35a23a05536bc632abb5c806e3b747db
Ketan <ketanmangukiya001@gmail.com>
2021-12-27 21:59:01 +0530

    collect static

--- Store_GO/settings.py
+++ Store_GO/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-uvu)8gb9ow-ul#cw43m@(3mgl=kqsny)+_+sx!6duq=ct)*m!p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -124,9 +124,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
